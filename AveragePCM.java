145f1fabc0fe677ff0f7a45a5d46f385f6e8e0b0
N.Pradhanya <pn4123@srmist.edu.in>
2026-03-02 08:50:03 +0530

    Add files via upload

---
+++ AveragePCM.java
@@ -0,0 +1,18 @@
+public class AveragePCM {
+    public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        double average = (maths + physics + chemistry) / 3.0;
+
+        System.out.println("Sam's average mark in PCM is " + average);
+    }
+} public class KmToMiles {
+    public static void main(String[] args) {
+        double km = 10.8;
+        double miles = km * 1.6;
+
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
+}
